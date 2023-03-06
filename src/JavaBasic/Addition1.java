08526db7101f9913ae9049b0815b9fbf0d7d1c3d
arfat132 <arfatbegum90@gmail.com>
2023-03-06 20:15:17 +0300

    addition with two numbers

---
+++ src/JavaBasic/Addition1.java
@@ -0,0 +1,16 @@
+package JavaBasic;
+// A small program to add 2 integers
+public class Addition1 {
+    public static void main(String[] args) {
+// declaration
+        int number1; // first number to add
+        int number2; // second number to add
+        int sum; // sum of number1 and number2
+// initialization
+        number1 = 45;
+        number2 = 72;
+// add numbers, then store total in sum
+        sum = number1 + number2;
+        System.out.println("Sum is " + sum); // display sum
+    } // end main method
+} // end class Addition1
