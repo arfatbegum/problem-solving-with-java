d2e2d1a8e283e0c069ebd008c26edad586313870
arfat132 <arfatbegum90@gmail.com>
2023-03-07 20:25:10 +0300

    computes the area and perimeter of a triangle

---
+++ src/ExamQuestions/Triangle.java
@@ -0,0 +1,19 @@
+package ExamQuestions;
+
+import java.util.Scanner;
+
+public class Triangle {
+    public static void main(String[]args){
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the 3 sides of a triangle: ");
+        int a = input.nextInt();
+        int b = input.nextInt();
+        int c = input.nextInt();
+        double s = (a+b+c)/2;
+        double area = Math.sqrt(s*(s-a)*(s-b)*(s-c));
+        double perimeter=a+b+c;
+        System.out.printf("Area= %.2f\n", area);
+        System.out.printf("perimeter= %.2f\n ", perimeter);
+
+    }
+}
